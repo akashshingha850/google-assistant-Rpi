3298e320767cc7762e4c7d421880af0fbcf1b1ae
Akash Shingha <akashshingha850@gmail.com>
2020-02-16 04:30:07 +0600

    Add files via upload

---
+++ download.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  DATA_DIR="/home/pi/google_assistant"
+else
+  DATA_DIR="$1"
+fi
+
+# Install required packages
+sudo apt-get install portaudio19-dev libffi-dev libssl-dev
+pip install --upgrade google-assistant-sdk[samples]
+pip install RPi.GPIO
+
+
+
+echo -e "Files downloaded to ${DATA_DIR}"
